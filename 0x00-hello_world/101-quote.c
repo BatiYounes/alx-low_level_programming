c97f218d6d1c7a58189fac3b073a256da0eca3b9
Bati Younes <younesbati007@gmail.com>
2023-07-14 14:06:05 -0700

    trying to betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,16 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific quote to the standard error
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t length = sizeof(message) - 1;
-    ssize_t bytes_written = write(2, message, length);
-    return (bytes_written == length) ? 1 : -1;
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t length = sizeof(message) - 1;
+	ssize_t bytes_written = write(2, message, length);
+
+	return ((bytes_written == length) ? 1 : -1);
 }
