fd802ac4b7082fcd035507784817504e5e26cf29
Bati Younes <younesbati007@gmail.com>
2023-07-15 07:18:31 -0700

    ff

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
  */
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char message[] =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t length = sizeof(message) - 1;
 	ssize_t bytes_written = write(2, message, length);
 
