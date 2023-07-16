c587374abb78445d0e7def47f028597c27dc0c37
Bati Younes <younesbati007@gmail.com>
2023-07-16 06:50:39 -0700

    100-intel task 7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,14 @@
-#include <unistd.h>
-
+#include &lt;unistd.h&gt;
+#define MESSAGE &quot;and that piece of art is useful\&quot; - Dora Korpar, 2015-10-19\n&quot;
 /**
- * main - Entry point
- *
- * Description: Prints a specific quote to the standard error
- * Return: Always 1 (Error)
- */
+* main - Entry point of the program
+*
+* Description: Prints a specific message to standard error
+*
+* Return: 1
+*/
 int main(void)
 {
-	const char message[] =
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t length = sizeof(message) - 1;
-	ssize_t bytes_written = write(2, message, length);
-
-	return ((bytes_written == length) ? 1 : -1);
+write(2, MESSAGE, sizeof(MESSAGE) - 1);
+return (1);
 }
