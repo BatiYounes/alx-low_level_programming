f9538dfcfb9e4c7a015a57866009b354db00a6d2
Bati Younes <younesbati007@gmail.com>
2023-07-17 08:18:35 -0700

    x

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,28 +7,27 @@
  */
 int main(void)
 {
-	int hundreds_digit, tens_digit, ones_digit;
+	int num1, num2;
 
-	for (hundreds_digit = 0; hundreds_digit <= 7; hundreds_digit++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (tens_digit = hundreds_digit + 1; tens_digit <= 8; tens_digit++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			for (ones_digit = tens_digit + 1; ones_digit <= 9; ones_digit++)
-			{
-				putchar(hundreds_digit + '0');
-				putchar(tens_digit + '0');
-				putchar(ones_digit + '0');
+			putchar(num1 / 10 + '0');
+			putchar(num1 % 10 + '0');
+			putchar(' ');
+			putchar(num2 / 10 + '0');
+			putchar(num2 % 10 + '0');
 
-				if (hundreds_digit != 7 || tens_digit != 8 || ones_digit != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+			if (num1 != 98 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
 
 	putchar('\n');
 
-	return (0);
+	return 0;
 }
