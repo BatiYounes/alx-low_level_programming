c2ec47d2d93d50f3fb3953170d394921a9c190b7
Bati Younes <younesbati007@gmail.com>
2023-07-17 08:14:06 -0700

    fixing betty style problem

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,28 +7,28 @@
  */
 int main(void)
 {
-    int hundreds_digit, tens_digit, ones_digit;
+	int hundreds_digit, tens_digit, ones_digit;
 
-    for (hundreds_digit = 0; hundreds_digit <= 7; hundreds_digit++)
-    {
-        for (tens_digit = hundreds_digit + 1; tens_digit <= 8; tens_digit++)
-        {
-            for (ones_digit = tens_digit + 1; ones_digit <= 9; ones_digit++)
-            {
-                putchar(hundreds_digit + '0');
-                putchar(tens_digit + '0');
-                putchar(ones_digit + '0');
+	for (hundreds_digit = 0; hundreds_digit <= 7; hundreds_digit++)
+	{
+		for (tens_digit = hundreds_digit + 1; tens_digit <= 8; tens_digit++)
+		{
+			for (ones_digit = tens_digit + 1; ones_digit <= 9; ones_digit++)
+			{
+				putchar(hundreds_digit + '0');
+				putchar(tens_digit + '0');
+				putchar(ones_digit + '0');
 
-                if (hundreds_digit != 7 || tens_digit != 8 || ones_digit != 9)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
+				if (hundreds_digit != 7 || tens_digit != 8 || ones_digit != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
