20593d2eb65fd0fb43f7174fe5f67be79d3cf1ba
Bati Younes <younesbati007@gmail.com>
2023-07-17 03:26:30 -0700

    3rd task Printing alphabets in lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphab;
+
+	alphab = 'a';
+	while (alphab <= 'z')
+	{
+		putchar(alphab);
+		alphab++;
+	}
+
+	alphab = 'A';
+	while (alphab <= 'Z')
+	{
+		putchar(alphab);
+		alphab++;
+	}
+
+	putchar('\n');
+
+	return 0;
+}
