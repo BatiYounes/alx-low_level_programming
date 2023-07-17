972763404e7d407b947c195d846f0ebce326c597
Bati Younes <younesbati007@gmail.com>
2023-07-17 07:57:54 -0700

    fixing betty problem

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char ch;
+	char ch;
 
-    for (ch = '0'; ch <= '9'; ch++)
-        putchar(ch);
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
 
-    for (ch = 'a'; ch <= 'f'; ch++)
-        putchar(ch);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
 
-    putchar('\n');
+	putchar('\n');
 
-    return 0;
+	return (0);
 }
