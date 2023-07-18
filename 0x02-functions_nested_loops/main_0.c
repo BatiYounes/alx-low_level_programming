89b4a2db2cf5c95f5681cacdad2b28e6fc98ae52
Bati Younes <younesbati007@gmail.com>
2023-07-18 16:34:16 -0700

    fixing the compiling problem

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -10,10 +10,17 @@ int main(void)
 {
 	int r;
 
+	/* print_last_digit */
+	r = print_last_digit(98);
+	_putchar('0' + r);
+	_putchar('\n');
+
+	/* print_sign */
 	r = print_sign(98);
 	if (r < 0)
 		r *= -1;
 	_putchar(r + '0');
 	_putchar('\n');
+
 	return (0);
 }
