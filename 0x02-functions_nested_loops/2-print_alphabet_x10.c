b86ca69fe9901f9eaffa7015046e97b3ac427c2d
Bati Younes <younesbati007@gmail.com>
2023-07-18 07:31:41 -0700

    fixed the compilation problem

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,22 @@
 #include "10-main.h"
 
+/* Declaration of _putchar function */
+int _putchar(char c);
+
 /**
  * print_alphabet_x10 - Prints the alphabet in lowercase 10 times.
  */
 void print_alphabet_x10(void)
 {
-	char letter;
-	int i;
+    char letter;
+    int i;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			_putchar(letter);
-		}
-		_putchar('\n');
-	}
+    for (i = 0; i < 10; i++)
+    {
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
+            _putchar(letter);
+        }
+        _putchar('\n');
+    }
 }
