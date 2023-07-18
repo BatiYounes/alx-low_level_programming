be43f6e459b94890483b981444684a06665914f0
Bati Younes <younesbati007@gmail.com>
2023-07-18 06:55:34 -0700

    making sure the putchar is compiled

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "_putchar.h"
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -9,15 +9,14 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+    char str[] = "_putchar\n";
+    int i = 0;
 
-	return (0);
+    while (str[i] != '\0')
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+
+    return (0);
 }
