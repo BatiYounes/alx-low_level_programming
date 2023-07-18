ad27231324468cf48d7e6a744d565f0b2c2ca421
Bati Younes <younesbati007@gmail.com>
2023-07-18 15:34:53 -0700

    trying

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
-void _putchar(char c); // Declaration of _putchar function
+#include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet in lowercase ten times
+ * print_alphabet_x10 - Prints the alphabet in lowercase ten times.
  */
 void print_alphabet_x10(void)
 {
-    char letter;
-    int i;
+	char letter;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-            _putchar(letter);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
 }
