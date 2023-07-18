7d2369530128a8c26b3e3d0a98077bf5eb0acdcd
Bati Younes <younesbati007@gmail.com>
2023-07-18 01:35:20 -0700

    t

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,17 @@
-#include &lt;unistd.h&gt;
-#define MESSAGE &quot;and that piece of art is useful\&quot; - Dora Korpar, 2015-10-19\n&quot;
+#include <unistd.h>
+
 /**
-* main - Entry point of the program
-*
-* Description: Prints a specific message to standard error
-*
-* Return: 1
-*/
+ * main - Entry point of the program
+ *
+ * Description: Prints a specific string to the standard error
+ * Return: Always 1 (indicating an error)
+ */
 int main(void)
 {
-write(2, MESSAGE, sizeof(MESSAGE) - 1);
-return (1);
+    ssize_t len = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n") - 1;
+
+    /* Write the string to the standard error */
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+
+    return (1);
 }
