727327297138e23cd0bebecbb4c2dc1b8383d11b
Bati Younes <younesbati007@gmail.com>
2023-07-21 07:14:02 -0700

    trying to check the code in intranet with warnning in betty coding style output

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14, followed by a new line.
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (j >= 10)
+				_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+		}
+
+		for (j = 10; j <= 14; j++)
+		{
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+		}
+
+		_putchar('\n');
+	}
+}
