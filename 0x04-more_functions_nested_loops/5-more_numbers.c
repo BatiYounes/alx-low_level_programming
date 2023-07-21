07d0746d4f375a40073f383aa4184baa699bd1f8
Bati Younes <younesbati007@gmail.com>
2023-07-21 07:26:09 -0700

    trying new compilation, probably still having a problem in the betty coding

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,12 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 				_putchar('0' + j / 10);
 			_putchar('0' + j % 10);
 		}
-
-		for (j = 10; j <= 14; j++)
-		{
-			_putchar('0' + j / 10);
-			_putchar('0' + j % 10);
-		}
-
 		_putchar('\n');
 	}
 }
