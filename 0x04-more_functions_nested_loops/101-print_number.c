40c5b0a8384605cb5ae6658ca0c717ea8fd83740
Bati Younes <younesbati007@gmail.com>
2023-07-22 13:14:21 -0700

    having error 7 8 9

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,36 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer
- * @n: The integer to print
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
 void print_number(int n)
 {
+	int last_digit, reversed;
+
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-	if (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10 != 0)
-		print_number(n / 10);
-	_putchar((n % 10) + '0');
+
+	reversed = 0;
+	while (n != 0)
+	{
+		last_digit = n % 10;
+		reversed = reversed * 10 + last_digit;
+		n /= 10;
+	}
+
+	while (reversed != 0)
+	{
+		last_digit = reversed % 10;
+		_putchar(last_digit + '0');
+		reversed /= 10;
+	}
 }
