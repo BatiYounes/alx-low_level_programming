0784f3c6db6f7c20dde87e8cd66bfe2288a3535f
Bati Younes <younesbati007@gmail.com>
2023-07-24 03:25:36 -0700

    x

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,10 @@
  */
 void puts2(char *str)
 {
-	int length = _strlen(str);
-	int i;
+    int length = _strlen(str);
+    int i;
 
-	for (i = 0; i < length; i += 2)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+    for (i = 0; i < length; i += 2)
+        _putchar(str[i]);
+    _putchar('\n');
 }
