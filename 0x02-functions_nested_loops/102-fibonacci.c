08414dce560b0a55e89b87c1e24d85998bc672c0
Bati Younes <younesbati007@gmail.com>
2023-07-25 03:48:27 -0700

    fibo assignment

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_fibonacci - Prints the first 50 Fibonacci numbers.
+ */
+void print_fibonacci(void)
+{
+	int i;
+	unsigned long int fib1 = 1, fib2 = 2, next_fib;
+
+	printf("%lu, %lu", fib1, fib2);
+
+	for (i = 2; i < 50; i++)
+	{
+		next_fib = fib1 + fib2;
+		printf(", %lu", next_fib);
+
+		fib1 = fib2;
+		fib2 = next_fib;
+	}
+
+	printf("\n");
+}
