1a81aedb0eef2804b33a2654db73f3b4daa7c365
Bati Younes <younesbati007@gmail.com>
2023-07-27 02:56:09 -0700

    tryint to fix bugs 1st task advanced

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,19 +8,17 @@
  */
 char *rot13(char *str)
 {
-    int i;
+	int i;
 
-    for (i = 0; str[i]; i++)
-    {
-        if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
-        {
-            str[i] += 13;
-        }
-        else if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
-        {
-            str[i] -= 13;
-        }
-    }
+	for (i = 0; str[i]; i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'm') ||
+		    (str[i] >= 'A' && str[i] <= 'M'))
+			str[i] += 13;
+		else if ((str[i] >= 'n' && str[i] <= 'z') ||
+			 (str[i] >= 'N' && str[i] <= 'Z'))
+			str[i] -= 13;
+	}
 
-    return str;
+	return (str);
 }
