702afe268555a09241255f262911fb34dabe0e80
Bati Younes <younesbati007@gmail.com>
2023-08-02 14:47:27 -0700

    6th task of the recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
-/* Function prototype */
-int calculate_sqrt(int n, int i);
-
 /**
  * _sqrt_recursion - Calculates the natural square root of a number.
  * @n: The number to calculate the square root of.
  *
- * Return: The natural square root of n. else, returns -1.
+ * Return: The natural square root of n. If n does not have a natural square root, returns -1.
  */
 int _sqrt_recursion(int n)
 {
@@ -17,21 +14,23 @@ int _sqrt_recursion(int n)
 	if (n == 0 || n == 1)
 		return (n); /* Base case: If n is 0 or 1, the square root is n */
 
-	return (calculate_sqrt(n, 1)); /* Start calculating the square root from 1 */
+	return (_calculate_sqrt(n, 1)); /* Start calculating the square root from 1 */
 }
 
 /**
- * calculate_sqrt - Helper function to calculate the square root.
+ * _calculate_sqrt - Helper function to calculate the square root.
  * @n: The number to calculate the square root of.
  * @i: The current integer to check if its square is equal to n.
+ *
+ * Return: The square root of n if found, otherwise, continue checking with the next integer.
  */
-int calculate_sqrt(int n, int i)
+int _calculate_sqrt(int n, int i)
 {
 	if (i * i == n)
 		return (i); /* If i*i is equal to n, we found the square root */
 
 	if (i * i > n)
-		return (-1); /*If i*i is greater than n, n don't have a natural square root*/
+		return (-1); /* If i*i is greater than n, n does not have a natural square root */
 
-	return (calculate_sqrt(n, i + 1)); /* Recur with the next integer */
+	return (_calculate_sqrt(n, i + 1)); /* Recur with the next integer */
 }
