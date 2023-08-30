e964504bfc6a98ba6f315339eb83ee8d6f5987c9
BatiYounes <younesbati007@gmail.com>
2023-08-30 02:42:15 -0700

    task advanced 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,139 +1,36 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_listint_ptrs2 - function that frees a listint_t list.
- * @head: pointer to the first and new node in the list.
- *
- * Return: nothing
+ * free_listint_safe - Frees a listint_t list.
+ * @h: A pointer to a pointer to the head of the list.
+ * Return: The size of the list that was freed.
  */
-
-void free_listint_ptrs2(listptr_t **head)
-{
-	listptr_t *current, *temp;
-
-	if (head != NULL)
-	{
-		current = *head;
-		while ((temp = current) != NULL)
-		{
-			current = current->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * free_listint_safe - function that frees a listint_t list.
- * @h: double pointer to the first node in the list.
- *
- * Return: the size of the list that was freed.
- */
-
 size_t free_listint_safe(listint_t **h)
 {
-	listptr_t *loop_tracker, *tracker_head, *tracker_node;
-	size_t num_nodes_freed = 0;
-	listint_t *current_node;
+	size_t count = 0;
+	listint_t *current, *temp;
 
-	tracker_head = NULL;
+	if (h == NULL)
+		return (0);
 
-	while (*h != NULL)
+	current = *h;
+	while (current != NULL)
 	{
-		loop_tracker = malloc(sizeof(listptr_t));
-
-		if (loop_tracker == NULL)
-			exit(98);
-		loop_tracker->ptr = (void *)*h;
-		loop_tracker->next = tracker_head;
-		tracker_head = loop_tracker;
-		tracker_node = tracker_head;
-
-		while (tracker_node->next != NULL)
+		count++;
+		if (current > current->next)
 		{
-			tracker_node = tracker_node->next;
-			if (*h == tracker_node->ptr)
-			{
-				*h = NULL;
-				free_listint_ptrs2(&tracker_head);
-				return (num_nodes_freed);
-			}
+			temp = current->next;
+			free(current);
+			current = temp;
 		}
-		current_node = *h;
-		*h = (*h)->next;
-		free(current_node);
-		num_nodes_freed++;
+		else
+		{
+			free(current);
+			break;
+		}
 	}
+
 	*h = NULL;
-	free_listint_ptrs2(&tracker_head);
-	return (num_nodes_freed);
-}#include "lists.h"
-
-/**
- * free_listint_ptrs2 - function that frees a listint_t list.
- * @head: pointer to the first and new node in the list.
- *
- * Return: nothing
- */
-
-void free_listint_ptrs2(listptr_t **head)
-{
-	listptr_t *current, *temp;
-
-	if (head != NULL)
-	{
-		current = *head;
-		while ((temp = current) != NULL)
-		{
-			current = current->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * free_listint_safe - function that frees a listint_t list.
- * @h: double pointer to the first node in the list.
- *
- * Return: the size of the list that was freed.
- */
-
-size_t free_listint_safe(listint_t **h)
-{
-	listptr_t *loop_tracker, *tracker_head, *tracker_node;
-	size_t num_nodes_freed = 0;
-	listint_t *current_node;
-
-	tracker_head = NULL;
-
-	while (*h != NULL)
-	{
-		loop_tracker = malloc(sizeof(listptr_t));
-
-		if (loop_tracker == NULL)
-			exit(98);
-		loop_tracker->ptr = (void *)*h;
-		loop_tracker->next = tracker_head;
-		tracker_head = loop_tracker;
-		tracker_node = tracker_head;
-
-		while (tracker_node->next != NULL)
-		{
-			tracker_node = tracker_node->next;
-			if (*h == tracker_node->ptr)
-			{
-				*h = NULL;
-				free_listint_ptrs2(&tracker_head);
-				return (num_nodes_freed);
-			}
-		}
-		current_node = *h;
-		*h = (*h)->next;
-		free(current_node);
-		num_nodes_freed++;
-	}
-	*h = NULL;
-	free_listint_ptrs2(&tracker_head);
-	return (num_nodes_freed);
+	return (count);
 }
--- 0x13-more_singly_linked_lists/102-main.c
+++ 0x13-more_singly_linked_lists/102-main.c
@@ -1,29 +1,12 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
     listint_t *head;
-    listint_t *head2;
     listint_t *node;
 
-    head2 = NULL;
-    add_nodeint(&head2, 0);
-    add_nodeint(&head2, 1);
-    add_nodeint(&head2, 2);
-    add_nodeint(&head2, 3);
-    add_nodeint(&head2, 4);
-    add_nodeint(&head2, 98);
-    add_nodeint(&head2, 402);
-    add_nodeint(&head2, 1024);
-    print_listint_safe(head2);
     head = NULL;
     node = add_nodeint(&head, 0);
     add_nodeint(&head, 1);
@@ -34,8 +17,8 @@ int main(void)
     add_nodeint(&head, 402);
     add_nodeint(&head, 1024);
     print_listint_safe(head);
-    free_listint_safe(&head2);
-    free_listint_safe(&head);
-    printf("%p, %p\n", (void *)head2, (void *)head);
+    printf("Size of the list: %lu\n", free_listint_safe(&head));
+    printf("Head after freeing: %p\n", (void *)head);
+
     return (0);
 }
