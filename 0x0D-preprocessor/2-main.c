1b3332b782f5661fad9ab0052bc15618b04991a8
BatiYounes <younesbati007@gmail.com>
2023-08-30 02:48:18 -0700

    test

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-#include "4-sum.h"
-#include "4-sum.h"
 
 /**
- * main - check the code
+ * main - prints the name of the file it was compiled from
  *
- * Return: Always 0.
+ * Return: 0 success
  */
+
 int main(void)
 {
-    int s;
-
-    s = SUM(98, 1024);
-    printf("%d\n", s);
-    return (0);
+	printf("%s\n", __BASE_FILE__);
+	return (0);
 }
-
