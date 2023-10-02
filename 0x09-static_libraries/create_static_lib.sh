fac7afa9be2606f116704a90627e4076bb5af769
BatiYounes <Younesbati007@gmail.com>
2023-10-02 16:55:20 -0700

    testing second task

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-
-# Create a temporary directory to store object files
-mkdir tmp_objects
-
-# Compile all .c files into .o files and store them in the temporary directory
-for file in *.c
-do
-  gcc -Wall -pedantic -Werror -Wextra -c "$file" -o "tmp_objects/${file%.c}.o"
+for file in *.c; do
+	if [[ -f $file ]]; then
+		gcc -c "$file" -o "${file%.c}.o"
+	fi
 done
-
-# Create the static library liball.a from all .o files in the temporary directory
-ar rcs liball.a tmp_objects/*.o
-
-# Clean up by removing the temporary directory
-rm -rf tmp_objects
+ar rc liball.a *.o
