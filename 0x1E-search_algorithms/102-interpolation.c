8edd366fe15498a0fff6e71a153fe5a0e6bd74de
Younesbati <younesbati007@gmail.come>
2024-07-17 08:00:43 -0700

    doing the task of Search algorithms

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,12 +14,12 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
+	if (array == NULL)
+		return (-1);
+
 	size_t low = 0;
 	size_t high = size - 1;
 	size_t pos;
-
-	if (array == NULL)
-		return (-1);
 
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
