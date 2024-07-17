f4ca3c569549f1f84055d30a3867fa7140e09f2a
Younesbati <younesbati007@gmail.come>
2024-07-17 08:28:31 -0700

    doing the task of Search algorithms

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,53 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * interpolation_search - Searches for a value in a sorted array using the
- *                        Interpolation search algorithm
+ * interpolation_search - Searches for a value in a sorted array of integers
+ *                        using Interpolation Search algorithm.
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in the array.
+ * @value: Value to search for.
  *
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in the array
- * @value: Value to search for
- *
- * Return: First index where value is located, or -1 if not present or
- *         array is NULL
+ * Return: Index of the first occurrence of value in array, or -1 if not found
+ *         or array is NULL.
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	size_t low = 0;
 	size_t high = size - 1;
-	size_t pos;
 
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
-		if (low == high)
-		{
-			if (array[low] == value)
-				return (low);
-			return (-1);
-		}
-
-		pos = low + (((double)(high - low) / (array[high] - array[low])) *
-		             (value - array[low]));
-
-		if (pos >= size)
-		{
-			printf("Value checked array[%lu] is out of range\n", pos);
-			return (-1);
-		}
+		size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
 
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
 		if (array[pos] == value)
-			return (pos);
+			return ((int)pos);
+
 		if (array[pos] < value)
 			low = pos + 1;
 		else
 			high = pos - 1;
 	}
 
+	printf("Value checked array[%lu] is out of range\n", low);
 	return (-1);
 }
