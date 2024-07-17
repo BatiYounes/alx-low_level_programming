075738d6e3cddb459c3432e0cde12866cdc5b188
Younesbati <younesbati007@gmail.come>
2024-07-17 03:18:56 -0700

    doing the task of Search algorithms

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -6,14 +6,18 @@
  * @array: Pointer to the first element of the array
  * @left: Starting index to print from
  * @right: Ending index to print to
+ *
+ * Description: Prints the elements of the array from index 'left' to 'right'.
  */
 void print_array(int *array, size_t left, size_t right)
 {
+	size_t i;
+
 	printf("Searching in array: ");
-	for (; left <= right; left++)
+	for (i = left; i <= right; i++)
 	{
-		printf("%d", array[left]);
-		if (left < right)
+		printf("%d", array[i]);
+		if (i < right)
 			printf(", ");
 	}
 	printf("\n");
@@ -25,12 +29,12 @@ void print_array(int *array, size_t left, size_t right)
  * @left: Starting index of the array
  * @right: Ending index of the array
  * @value: Value to search for
- * @orig_left: Original left index to adjust mid
- * @orig_right: Original right index to adjust mid
+ *
  * Return: Index where value is located, -1 if not found
+ *
+ * Description: Implements binary search recursively on the sorted array.
  */
-int adv_bin_recur(int *array, size_t left, size_t right, int value,
-		size_t orig_left, size_t orig_right)
+int adv_bin_recur(int *array, size_t left, size_t right, int value)
 {
 	size_t mid;
 
@@ -41,28 +45,31 @@ int adv_bin_recur(int *array, size_t left, size_t right, int value,
 
 		if (array[mid] == value)
 		{
-			if (mid == orig_left)
+			if (mid == 0 || array[mid - 1] != value)
 				return (mid);
-			print_array(array, left, mid);
-			return (adv_bin_recur(array, left, mid, value, orig_left, orig_right));
+			return (adv_bin_recur(array, left, mid, value));
 		}
-		if (array[mid] < value)
-			return (adv_bin_recur(array, mid + 1, right, value, orig_left, orig_right));
-		return (adv_bin_recur(array, left, mid, value, orig_left, orig_right));
+		else if (array[mid] < value)
+			return (adv_bin_recur(array, mid + 1, right, value));
+		else
+			return (adv_bin_recur(array, left, mid - 1, value));
 	}
 	return (-1);
 }
 
 /**
- * Searches for a value in a sorted array using advanced binary search
+ * advanced_binary - Searches for a value in a sorted array using advanced
  * @array: Pointer to the first element of the array
  * @size: Number of elements in array
  * @value: Value to search for
+ *
  * Return: Index where value is located, -1 if not found
+ *
+ * Description: Initiates the advanced binary search on the sorted array.
  */
 int advanced_binary(int *array, size_t size, int value)
 {
 	if (array == NULL || size == 0)
 		return (-1);
-	return (adv_bin_recur(array, 0, size - 1, value, 0, size - 1));
+	return (adv_bin_recur(array, 0, size - 1, value));
 }
